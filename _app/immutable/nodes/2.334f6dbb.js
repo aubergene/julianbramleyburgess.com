a200a12a70c559071c14ed50698f517a74620d7a
Julian Burgess <aubergene@gmail.com>
2023-06-12 23:54:47 +0100

    Updates

---
+++ _app/immutable/nodes/2.334f6dbb.js
@@ -0,0 +1,27 @@
+import{S as K,i as Q,s as R,k as s,q as i,a as q,l as r,m as o,r as l,h as n,c as M,n as f,b as U,C as t,D as N}from"../chunks/index.97b79440.js";function W(z){let h,p,e,w,_,y,b,C,x,k,E,A,a,P,m,B,D,u,I,S,g,T,j;return{c(){h=s("section"),p=s("div"),e=s("div"),w=s("p"),_=i(`Julian Bramley Burgess explores equilibria and geometry in sculptural and digital forms. His
+				work includes hanging mobile installations, mechanically-plotted prints, and paintings.`),y=q(),b=s("p"),C=i(`His mobiles emerge from whippletree structures formed from regular polygons. Finding balance
+				is a key part to this work, which looks at hierarchies and complex relationships. He uses
+				his background in software engineering to understand the principles and limits behind these
+				relations.`),x=q(),k=s("p"),E=i(`His prints and drawings build on ideas of dreams, entropy, and the collective unconscious.
+				In the work Dreamspace, he harvested over a hundred thousand dream diary entries. These were
+				then fed into a high-dimensional space using a machine learning algorithm. The resulting
+				semantic space was then projected and plotted in the familiar form of a stereographic
+				celestial map, where the stars are the objects of dreams and the constellations the links
+				between us all.`),A=q(),a=s("p"),P=i(`Burgess graduated with an MA in Computational Art from Goldsmiths College in 2020. His work
+				has been exhibited at the `),m=s("a"),B=i("Horniman Museum"),D=i(`, London
+				and `),u=s("a"),I=i("HoloCenter"),S=i(` Center for the Holographic Arts in New
+				York. His work is based at
+				`),g=s("a"),T=i("Proposition Studios"),j=i(" in Camden."),this.h()},l(v){h=r(v,"SECTION",{class:!0});var H=o(h);p=r(H,"DIV",{class:!0});var F=o(p);e=r(F,"DIV",{class:!0});var d=o(e);w=r(d,"P",{});var G=o(w);_=l(G,`Julian Bramley Burgess explores equilibria and geometry in sculptural and digital forms. His
+				work includes hanging mobile installations, mechanically-plotted prints, and paintings.`),G.forEach(n),y=M(d),b=r(d,"P",{});var J=o(b);C=l(J,`His mobiles emerge from whippletree structures formed from regular polygons. Finding balance
+				is a key part to this work, which looks at hierarchies and complex relationships. He uses
+				his background in software engineering to understand the principles and limits behind these
+				relations.`),J.forEach(n),x=M(d),k=r(d,"P",{});var L=o(k);E=l(L,`His prints and drawings build on ideas of dreams, entropy, and the collective unconscious.
+				In the work Dreamspace, he harvested over a hundred thousand dream diary entries. These were
+				then fed into a high-dimensional space using a machine learning algorithm. The resulting
+				semantic space was then projected and plotted in the familiar form of a stereographic
+				celestial map, where the stars are the objects of dreams and the constellations the links
+				between us all.`),L.forEach(n),A=M(d),a=r(d,"P",{});var c=o(a);P=l(c,`Burgess graduated with an MA in Computational Art from Goldsmiths College in 2020. His work
+				has been exhibited at the `),m=r(c,"A",{href:!0});var V=o(m);B=l(V,"Horniman Museum"),V.forEach(n),D=l(c,`, London
+				and `),u=r(c,"A",{href:!0});var Y=o(u);I=l(Y,"HoloCenter"),Y.forEach(n),S=l(c,` Center for the Holographic Arts in New
+				York. His work is based at
+				`),g=r(c,"A",{href:!0});var O=o(g);T=l(O,"Proposition Studios"),O.forEach(n),j=l(c," in Camden."),c.forEach(n),d.forEach(n),F.forEach(n),H.forEach(n),this.h()},h(){f(m,"href","https://www.horniman.ac.uk/"),f(u,"href","https://holocenter.org/"),f(g,"href","https://www.propositionstudios.com/"),f(e,"class","content"),f(p,"class","container is-max-desktop"),f(h,"class","section")},m(v,H){U(v,h,H),t(h,p),t(p,e),t(e,w),t(w,_),t(e,y),t(e,b),t(b,C),t(e,x),t(e,k),t(k,E),t(e,A),t(e,a),t(a,P),t(a,m),t(m,B),t(a,D),t(a,u),t(u,I),t(a,S),t(a,g),t(g,T),t(a,j)},p:N,i:N,o:N,d(v){v&&n(h)}}}class Z extends K{constructor(h){super(),Q(this,h,null,W,R,{})}}export{Z as component};
