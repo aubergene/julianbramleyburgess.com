74b37588e97810cc7df6ca381537889310a30ca6
Julian Burgess <aubergene@gmail.com>
2023-06-12 21:14:32 +0100

    initial bio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Julian Bramley Burgess</h1>
-<p>hello</p>
+<section class="hero has-background-white-ter">
+	<div class="hero-body">
+		<div class="container is-max-desktop">
+			<p class="title has-text-grey">Julian Bramley Burgess</p>
+		</div>
+	</div>
+</section>
+
+<section class="section">
+	<div class="container is-max-desktop">
+		<div class="content">
+			<p>
+				Julian Bramley Burgess explores equilibriums and geometric principles in an experimental
+				way. His work includes hanging mobile installations, mechanically and hand plotted prints
+				and projections of digitally produced constructions.
+			</p>
+
+			<p>
+				Currently he experiments with creating hanging whippletree structures formed from regular
+				polygons. Finding equilibriums and balance is a key part to this work which look at the
+				entangled behavour of how hierachies form complex relationships. He uses his background in
+				software engineering to understand the principals and limits behind these relations
+			</p>
+
+			<p>
+				His prints and drawings relate to his sculptural work. In the work Dreamspace he harvest
+				over a hundred thousand dream diary entries. These were then fed into a high dimensional
+				spacing using a machine learning algorithm. The resulting space was then reprojected and
+				plotted in the familiar form of a stereographic celestial map, where the stars are the
+				objects of dreams and the constellations the links between us all.
+			</p>
+
+			<p>
+				In 2020 Burgess graduated with an MA in Computational Art from Goldsmiths College. His work
+				has been exhibited at the <a href="https://www.horniman.ac.uk/">Horniman Museum</a>, London
+				and
+				<a href="https://holocenter.org/">HoloCenter</a>
+				– Center for the Holographic Arts in New York. His practice is based at
+				<a href="https://www.propositionstudios.com/">Proposition Studios</a> in Camden.
+			</p>
+		</div>
+	</div>
+</section>
