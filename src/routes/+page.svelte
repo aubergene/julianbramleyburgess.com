16c8499e38c6e6ee42c71fdfa915800bb833b12a
Julian Burgess <aubergene@gmail.com>
2023-06-12 23:54:39 +0100

    edit bio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,32 +2,31 @@
 	<div class="container is-max-desktop">
 		<div class="content">
 			<p>
-				Julian Bramley Burgess explores equilibriums and geometric principles in an experimental
-				way. His work includes hanging mobile installations, mechanically and hand plotted prints
-				and projections of digitally produced constructions.
+				Julian Bramley Burgess explores equilibria and geometry in sculptural and digital forms. His
+				work includes hanging mobile installations, mechanically-plotted prints, and paintings.
 			</p>
 
 			<p>
-				Currently he experiments with creating hanging whippletree structures formed from regular
-				polygons. Finding equilibriums and balance is a key part to this work which look at the
-				entangled behavour of how hierachies form complex relationships. He uses his background in
-				software engineering to understand the principals and limits behind these relations
+				His mobiles emerge from whippletree structures formed from regular polygons. Finding balance
+				is a key part to this work, which looks at hierarchies and complex relationships. He uses
+				his background in software engineering to understand the principles and limits behind these
+				relations.
 			</p>
 
 			<p>
-				His prints and drawings relate to his sculptural work. In the work Dreamspace he harvest
-				over a hundred thousand dream diary entries. These were then fed into a high dimensional
-				spacing using a machine learning algorithm. The resulting space was then reprojected and
-				plotted in the familiar form of a stereographic celestial map, where the stars are the
-				objects of dreams and the constellations the links between us all.
+				His prints and drawings build on ideas of dreams, entropy, and the collective unconscious.
+				In the work Dreamspace, he harvested over a hundred thousand dream diary entries. These were
+				then fed into a high-dimensional space using a machine learning algorithm. The resulting
+				semantic space was then projected and plotted in the familiar form of a stereographic
+				celestial map, where the stars are the objects of dreams and the constellations the links
+				between us all.
 			</p>
 
 			<p>
-				In 2020 Burgess graduated with an MA in Computational Art from Goldsmiths College. His work
+				Burgess graduated with an MA in Computational Art from Goldsmiths College in 2020. His work
 				has been exhibited at the <a href="https://www.horniman.ac.uk/">Horniman Museum</a>, London
-				and
-				<a href="https://holocenter.org/">HoloCenter</a>
-				– Center for the Holographic Arts in New York. His practice is based at
+				and <a href="https://holocenter.org/">HoloCenter</a> Center for the Holographic Arts in New
+				York. His work is based at
 				<a href="https://www.propositionstudios.com/">Proposition Studios</a> in Camden.
 			</p>
 		</div>
