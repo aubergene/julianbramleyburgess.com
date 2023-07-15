119e1f725989e4053c4935bd8e5eab5cafd12da7
Julian Burgess <aubergene@gmail.com>
2023-07-15 23:16:25 +0100

    link external

--- src/lib/components/NavLink.svelte
+++ src/lib/components/NavLink.svelte
@@ -3,15 +3,19 @@
 	import { page } from '$app/stores';
 
 	export let href;
+	export let target;
 
 	let active;
+	let external;
 	$: {
-		href = base + href;
+		external = href.startsWith('http');
+		target = external ? '_blank' : null;
+		href = external ? href : base + href;
 		active = $page.url.pathname === href;
 	}
 </script>
 
-<a {href} aria-current={active} class="navbar-item" class:is-active={active}><slot /></a>
+<a {href} aria-current={active} class="navbar-item" class:is-active={active} {target}><slot /></a>
 
 <style>
 	.is-active {
