07eb4189d7ac6d262094342f6d879bbcc3409e69
Julian Burgess <aubergene@gmail.com>
2023-07-15 13:18:42 +0100

    new bio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,32 +2,15 @@
 	<div class="container is-max-desktop">
 		<div class="content">
 			<p>
-				Julian Bramley Burgess explores equilibria and geometry in sculptural and digital forms. His
-				work includes hanging mobile installations, mechanically-plotted prints, and paintings.
-			</p>
-
-			<p>
-				His mobiles emerge from whippletree structures formed from regular polygons. Finding balance
-				is a key part to this work, which looks at hierarchies and complex relationships. He uses
-				his background in software engineering to understand the principles and limits behind these
-				relations.
-			</p>
-
-			<p>
-				His prints and drawings build on ideas of dreams, entropy, and the collective unconscious.
-				In the work Dreamspace, he harvested over a hundred thousand dream diary entries. These were
-				then fed into a high-dimensional space using a machine learning algorithm. The resulting
-				semantic space was then projected and plotted in the familiar form of a stereographic
-				celestial map, where the stars are the objects of dreams and the constellations the links
-				between us all.
-			</p>
-
-			<p>
-				Burgess graduated with an MA in Computational Art from Goldsmiths College in 2020. His work
-				has been exhibited at the <a href="https://www.horniman.ac.uk/">Horniman Museum</a>, London
-				and <a href="https://holocenter.org/">HoloCenter</a> Center for the Holographic Arts in New
-				York. His work is based at
-				<a href="https://www.propositionstudios.com/">Proposition Studios</a> in Camden.
+				Julian Bramley Burgess is a contemporary artist whose works explore the intricate
+				connections between equilibrium, geometry, and the human psyche. Through his captivating
+				artworks, Burgess invites viewers on an introspective journey, prompting thoughtful
+				reflection on the interplay of these elements. Drawing inspiration from the ethereal nature
+				of dreams and the delicate balance of chaos and order, his creations offer glimpses into a
+				realm where hierarchies dissolve and complex relationships intertwine. With a keen eye for
+				form and a deep understanding of the human experience, Burgess's art challenges conventional
+				boundaries, inviting viewers to question their perceptions and contemplate the mysteries of
+				existence.
 			</p>
 		</div>
 	</div>
