6f58a35a250ddfc91366032e28fe9747e34ba608
Julian Burgess <aubergene@gmail.com>
2023-07-15 23:16:37 +0100

    simple links

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -14,34 +14,11 @@
 	<div class="container is-max-desktop">
 		<div class="navbar-brand">
 			<NavLink href="/">About</NavLink>
-			<a
-				on:click|stopPropagation={() => (burgerActive = !burgerActive)}
-				role="button"
-				class="navbar-burger"
-				class:is-active={burgerActive}
-				aria-label="menu"
-				aria-expanded="false"
-				data-target="navbarBasicExample"
-			>
-				<span aria-hidden="true" />
-				<span aria-hidden="true" />
-				<span aria-hidden="true" />
-			</a>
-		</div>
-
-		<div id="navbarBasicExample" class="navbar-menu" class:is-active={burgerActive}>
-			<div class="navbar-start">
-				<NavLink href="/works">Works</NavLink>
-				<NavLink href="/cv">CV</NavLink>
-			</div>
-		</div>
-
-		<div class="navbar-end">
-			<div class="navbar-item">
-				<NavLink href="https://www.instagram.com/julianbramleyburgess/">
-					<img src="/instagram.svg" alt="Instagram" />
-				</NavLink>
-			</div>
+			<NavLink href="/works">Works</NavLink>
+			<NavLink href="/cv">CV</NavLink>
+			<NavLink href="https://www.instagram.com/julianbramleyburgess/">
+				<img src="/instagram.svg" alt="Instagram" />
+			</NavLink>
 		</div>
 	</div>
 </nav>
