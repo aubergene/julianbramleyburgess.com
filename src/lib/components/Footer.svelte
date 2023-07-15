d67d3ccf025d49c72c674ce17738f8a489cca48e
Julian Burgess <aubergene@gmail.com>
2023-07-15 23:16:13 +0100

    add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,11 @@
+<footer class="footer">
+	<div class="content has-text-centered">
+		<p>
+			<a href="https://www.instagram.com/julianbramleyburgess/">
+				<img src="/instagram.svg" alt="Instagram" style="vertical-align: middle; opacity: 0.25;" /> julianbramleyburgess</a
+			>
+		</p>
+
+		<p>&copy; 2023 Julian Bramley Burgess</p>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import Nav from '$lib/components/Nav.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Header />
 <Nav />
 
 <slot />
+
+<Footer />
 
 <style>
 	:global(.content, .title) {
