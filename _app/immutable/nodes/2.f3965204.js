1249f7622c80abf4b222c52921b8b01534a67953
Julian Burgess <aubergene@gmail.com>
2023-07-24 01:29:01 +0100

    Updates

---
+++ _app/immutable/nodes/2.f3965204.js
@@ -0,0 +1,17 @@
+import{S as P,i as W,s as z,k as e,a as _,q as A,y as G,l as n,m as d,h as l,c as y,r as N,z as O,n as a,L as F,b as H,C as t,A as K,D as Q,g as U,d as X,B as Y}from"../chunks/index.c9f2c2cb.js";import{M as Z}from"../chunks/MailingList.d3a441fb.js";function tt(L){let r,c,h,p,i,J,D,f,g,v,E,k,s,I,$,q,V,j,C,u,w;return u=new Z({}),{c(){r=e("section"),c=e("div"),h=e("div"),p=e("div"),i=e("img"),D=_(),f=e("div"),g=e("div"),v=e("p"),E=A(`Julian Bramley Burgess is a contemporary artist whose works explore the intricate
+						connections between equilibrium, geometry, and the human psyche. Through his captivating
+						artworks, Burgess invites viewers on an introspective journey, prompting thoughtful
+						reflection on the interplay of these elements. Drawing inspiration from the ethereal
+						nature of dreams and the delicate balance of chaos and order, his creations offer
+						glimpses into a realm where hierarchies dissolve and complex relationships intertwine.
+						With a keen eye for form and a deep understanding of the human experience, Burgess's art
+						challenges conventional boundaries, inviting viewers to question their perceptions and
+						contemplate the mysteries of existence.`),k=_(),s=e("div"),I=e("br"),$=_(),q=e("br"),V=_(),j=e("br"),C=_(),G(u.$$.fragment),this.h()},l(o){r=n(o,"SECTION",{class:!0});var b=d(r);c=n(b,"DIV",{class:!0});var x=d(c);h=n(x,"DIV",{class:!0});var B=d(h);p=n(B,"DIV",{class:!0});var M=d(p);i=n(M,"IMG",{class:!0,width:!0,height:!0,src:!0,alt:!0}),M.forEach(l),D=y(B),f=n(B,"DIV",{class:!0});var R=d(f);g=n(R,"DIV",{class:!0});var S=d(g);v=n(S,"P",{});var T=d(v);E=N(T,`Julian Bramley Burgess is a contemporary artist whose works explore the intricate
+						connections between equilibrium, geometry, and the human psyche. Through his captivating
+						artworks, Burgess invites viewers on an introspective journey, prompting thoughtful
+						reflection on the interplay of these elements. Drawing inspiration from the ethereal
+						nature of dreams and the delicate balance of chaos and order, his creations offer
+						glimpses into a realm where hierarchies dissolve and complex relationships intertwine.
+						With a keen eye for form and a deep understanding of the human experience, Burgess's art
+						challenges conventional boundaries, inviting viewers to question their perceptions and
+						contemplate the mysteries of existence.`),T.forEach(l),S.forEach(l),R.forEach(l),B.forEach(l),k=y(x),s=n(x,"DIV",{});var m=d(s);I=n(m,"BR",{}),$=y(m),q=n(m,"BR",{}),V=y(m),j=n(m,"BR",{}),C=y(m),O(u.$$.fragment,m),m.forEach(l),x.forEach(l),b.forEach(l),this.h()},h(){a(i,"class","has-ratio"),a(i,"width","1024"),a(i,"height","768"),F(i.src,J="/img/julian-bramley-burgess-1024.jpg")||a(i,"src",J),a(i,"alt","Julian Bramley Burgess"),a(p,"class","column"),a(g,"class","content"),a(f,"class","column"),a(h,"class","columns"),a(c,"class","container is-max-desktop"),a(r,"class","section")},m(o,b){H(o,r,b),t(r,c),t(c,h),t(h,p),t(p,i),t(h,D),t(h,f),t(f,g),t(g,v),t(v,E),t(c,k),t(c,s),t(s,I),t(s,$),t(s,q),t(s,V),t(s,j),t(s,C),K(u,s,null),w=!0},p:Q,i(o){w||(U(u.$$.fragment,o),w=!0)},o(o){X(u.$$.fragment,o),w=!1},d(o){o&&l(r),Y(u)}}}class st extends P{constructor(r){super(),W(this,r,null,tt,z,{})}}export{st as component};
