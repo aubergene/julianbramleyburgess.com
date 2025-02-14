73ae4cf03ed2e0bdea4b1f48e74ad8993a45b23c
Julian Burgess <aubergene@gmail.com>
2025-02-14 23:30:38 +0000

    defaults from scaffold

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	runes: true,
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	},
 
-	preprocess: [vitePreprocess({})]
+	preprocess: [vitePreprocess({ script: true }), mdsvex()],
+
+	extensions: ['.svelte', '.svx'],
 };
 
 export default config;
